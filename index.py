55268cab7868f8c0e59a8ee5ab9d37566febe7c9
BeatrizMariaPessoa <124363350+BeatrizMariaPessoa@users.noreply.github.com>
2023-12-29 11:53:43 -0300

    Update index.py

--- index.py
+++ index.py
@@ -10,23 +10,42 @@ from templates.editar_infos_geraisUI import EditarInfosUI
 from templates.ManterCidadeUI import ManterCidadeUI
 from templates.ManterEstadoUI import ManterEstadoUI
 from templates.ManterPaisUI import ManterPaisUI
+from templates.abrircontaUI import AbrirContaUI
 
 
 class IndexUI:
-    def não_logado():
-        op = st.sidebar.selectbox("Menu", ["Cadastre-Se","Loguin","ManterUserUI",
-"ManterPaisUI","ManterEstadoUI","ManterCidadeUI","Editar Perfil","Buscas"
-,"Comparar Paises", "Comparar Estados", "Comparar Cidades", "EditarInformacoes"])
-        if op == "Cadastre-Se": Cadastrar_seUI.main()
-        if op == "Editar Perfil": EditarPerfilUI.main()
-        if op == "Loguin": LoginUI.main()
-        if op == "Comparar Paises": CompararPisesUI.main()
-        if op == "Comparar Estados": CompararEstadosUI.main()
-        if op == "Comparar Cidades": CompararCidadesUI.main()
-        if op == "Buscas": BuscasUI.main()
-        if op == "EditarInformacoes": EditarInfosUI.main()
-        if op == "ManterPaisUI": ManterPaisUI.main()
-        if op == "ManterEstadoUI": ManterEstadoUI.main()
-        if op == "ManterCidadeUI": ManterCidadeUI.main()
-op = "Loguin"
+        def menu_visitante():
+                op = st.sidebar.selectbox("Menu", ["Login", "Abrir Conta"])
+                if op == "Login": LoginUI.main()
+                if op == "Abrir Conta": AbrirContaUI.main() 
+                
+        def menu_admin():
+                op = st.sidebar.selectbox("Menu", ["ManterUserUI", "ManterPaisUI", "ManterEstadoUI", "ManterCidadeUI", "Editar Perfil", "EditarInformacoes",])
+                if op == "ManterPaisUI": ManterPaisUI.main()
+                if op == "ManterEstadoUI": ManterEstadoUI.main()
+                if op == "ManterCidadeUI": ManterCidadeUI.main()
+                if op == "Editar Perfil": EditarPerfilUI.main()
+                if op == "EditarInformacoes": EditarInfosUI.main()
+
+        def menu_usuario():
+                op = st.sidebar.selectbox("Menu", ["Editar Perfil", "Buscas", "Comparar Paises", "Comparar Estados", "Comparar Cidades"])
+        
+
+        def não_logado():
+                op = st.sidebar.selectbox("Menu", ["Cadastre-Se","Loguin","ManterUserUI",
+        "ManterPaisUI","ManterEstadoUI","ManterCidadeUI","Editar Perfil","Buscas"
+        ,"Comparar Paises", "Comparar Estados", "Comparar Cidades", "EditarInformacoes"])
+                if op == "Cadastre-Se": Cadastrar_seUI.main()
+                if op == "Editar Perfil": EditarPerfilUI.main()
+                if op == "Loguin": LoginUI.main()
+                if op == "Comparar Paises": CompararPisesUI.main()
+                if op == "Comparar Estados": CompararEstadosUI.main()
+                if op == "Comparar Cidades": CompararCidadesUI.main()
+                if op == "Buscas": BuscasUI.main()
+                if op == "EditarInformacoes": EditarInfosUI.main()
+                if op == "ManterPaisUI": ManterPaisUI.main()
+                if op == "ManterEstadoUI": ManterEstadoUI.main()
+                if op == "ManterCidadeUI": ManterCidadeUI.main()
+        op = "Loguin"
+
 IndexUI.não_logado()
